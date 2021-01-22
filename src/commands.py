f8a5698e5e851628e19d78a0859a97b59da0fe53
TheSlam <32799244+salmmanfred@users.noreply.github.com>
2021-01-22 13:04:30 +0100

    added the simplest of shop interfaces

--- src/commands.py
+++ src/commands.py
@@ -48,7 +48,7 @@ _add_command(_ping)
 
 @commands.command(name="shop")
 async def _shop(ctx: Context, *args, **kwargs):
-    await shop.bounce_back(ctx)
+    await shop.shop(ctx)
 
 _add_command(_shop)
 
--- src/economy/shop.py
+++ src/economy/shop.py
@@ -1,10 +1,15 @@
 import discord
-
+import json
 async def bounce_back(ctx):
     await ctx.send("bounce")
 
 async def shop(ctx):
-    embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
-    for x in range(10):
-        embedVar.add_field(name="Field"+str(x), value="hi", inline=False)
-    ctx.channel.send(embedVar)
+    embedVar = discord.Embed(title="SHOP", description='"A shop that will suit all your needs"', color=0x00ff00)
+    shopjson = json.load(open("economy/shop.json","r"))
+    if len(shopjson) >= 10:
+        return 
+    for x in shopjson:
+        embedVar.add_field(name=x["emoji"]+" "+x["name"], value=x["val"], inline=False)
+    msg = await ctx.send(embed=embedVar)
+    #emoji = '\N{REGIONAL INDICATOR E}'
+    await msg.add_reaction("\:regional_indicator_a:")
