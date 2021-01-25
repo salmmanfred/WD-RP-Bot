91786ef22dd7822000d8d605f13b2af59cdbe1bc
TheSlam <32799244+salmmanfred@users.noreply.github.com>
2021-01-25 21:20:45 +0100

    pep

--- src/economy/stockmarket.py
+++ src/economy/stockmarket.py
@@ -6,6 +6,4 @@ Stockmarket plans:
 2. there is gonna be an option for buying it and selling it (represented with a blue for buying and a red for selling).
 3. the price(for the stock) is determined by the ammount sold compared to the day before is it more the price goes up is it 
 less it goes down.
-
-
 """
