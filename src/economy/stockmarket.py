6c78c4a10f3c259406bf228eb2d29fd0a6ce83e5
TheSlam <32799244+salmmanfred@users.noreply.github.com>
2021-01-25 21:22:00 +0100

    pep

--- src/economy/stockmarket.py
+++ src/economy/stockmarket.py
@@ -4,6 +4,6 @@ Stockmarket plans:
     1. gun stock.
     2. ammo stock.
 2. there is gonna be an option for buying it and selling it (represented with a blue for buying and a red for selling).
-3. the price(for the stock) is determined by the ammount sold compared to the day before is it more the price goes up is it 
+3. the price(for the stock) is determined by the ammount sold compared to the day before is it more the price goes up is it.
 less it goes down.
 """
